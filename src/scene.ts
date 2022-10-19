bcf7906aba68ea15941894209e6813b5c2d5eda6
Chris Owen <c.ornine@gmail.com>
2022-10-20 00:31:18 +0100

    chore: add comments, format file

--- src/scene.ts
+++ src/scene.ts
@@ -1,23 +1,31 @@
-import { Engine, Vector3, Color4, HemisphericLight, Scene, ArcRotateCamera } from "babylonjs";
+import {
+  Engine,
+  Vector3,
+  Color4,
+  HemisphericLight,
+  Scene,
+  ArcRotateCamera,
+} from "babylonjs";
 import { canvas } from "./domItems";
 
-export const engine = new Engine(canvas, true)
+export const engine = new Engine(canvas, true);
 
 export const scene = makeScene();
 
-function makeScene():Scene {
-  const scene = new Scene(engine)
-  createCamera(scene)
-  createLight(scene)
-  createBackground(scene)
-  return scene
+function makeScene(): Scene {
+  const scene = new Scene(engine);
+  createCamera(scene);
+  createLight(scene);
+  createBackground(scene);
+  return scene;
 }
 
-function createCamera(scene:Scene):void {
-  const alpha = Math.PI / 4
-  const beta = Math.PI / 3
-  const radius = 8
-  const target = new Vector3(0,0,0)
+function createCamera(scene: Scene): void {
+  //TODO - look at different cameras and how to control them
+  const alpha = Math.PI / 4;
+  const beta = Math.PI / 3;
+  const radius = 8;
+  const target = new Vector3(0, 0, 0);
 
   new ArcRotateCamera(
     "Camera",
@@ -26,17 +34,13 @@ function createCamera(scene:Scene):void {
     radius,
     target,
     scene
-  ).attachControl(canvas,true)
+  ).attachControl(canvas, true);
 }
 
-function createLight(scene:Scene):void {
-  new HemisphericLight(
-    "light",
-    new Vector3(1,1,0),
-    scene
-  )
+function createLight(scene: Scene): void {
+  new HemisphericLight("light", new Vector3(1, 1, 0), scene);
 }
 
-function createBackground(scene:Scene):void {
-  scene.clearColor = Color4.FromHexString("#23147a")
+function createBackground(scene: Scene): void {
+  scene.clearColor = Color4.FromHexString("#23147a");
 }
